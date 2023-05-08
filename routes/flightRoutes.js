6c3d6d703ed4ec30411e90f71bd8ac0893b7efc6
Gokul-Mylsami <gokulmylsami@gmail.com>
2023-05-08 21:11:33 +0530

    protected routes

--- routes/flightRoutes.js
+++ routes/flightRoutes.js
@@ -2,13 +2,28 @@ const express = require("express");
 const router = express.Router();
 
 const flightController = require("../controller/flightController");
-
+const userController = require("../controller/userController");
 router.get("/", flightController.getAllFlights);
 router.get("/airports", flightController.getAirports);
-router.post("/", flightController.createFlight);
+router.post(
+  "/",
+  userController.protect,
+  userController.restrictTo("admin"),
+  flightController.createFlight
+);
 
 router.get("/:id", flightController.getFlight);
-router.patch("/:id", flightController.updateFlight);
-router.delete("/:id", flightController.deleteFlight);
+router.patch(
+  "/:id",
+  userController.protect,
+  userController("admin"),
+  flightController.updateFlight
+);
+router.delete(
+  "/:id",
+  userController.protect,
+  userController.restrictTo("admin"),
+  flightController.deleteFlight
+);
 
 module.exports = router;
